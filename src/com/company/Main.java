c41fe1085cde443ed8e10a4a42a823fc97fd59fb
Ciupe Mihai <ciupe_mihai10@yahoo.com>
2020-12-09 23:51:35 +0200

    initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("1. Hello");
+
+     System.out.println("1. Hello");
         System.out.println("Mihai");
 
     // Sum of two numbers
