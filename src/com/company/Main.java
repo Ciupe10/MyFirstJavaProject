43b6daefdbd8c22fc8fd1c997ae19515f3b78f71
Ciupe Mihai <ciupe_mihai10@yahoo.com>
2020-12-09 14:50:39 +0200

    initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Mihai");
+        System.out.println("Hello");
+        System.out.println("Mihai");
+
+    // Sum of two numbers
+        int num1 = 456;
+        int num2 = 4356;
+        int sum = num1+num2;
+        System.out.println("Suma numerelor 456 si 4356 este " + sum);
+
+    // Dividing two numbers
+
+
+
+
+
+
+
 
 
 
