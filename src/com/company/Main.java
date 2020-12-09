1f7d24b73c0430a7f8156baf6de63bb88b7b4a28
Ciupe Mihai <ciupe_mihai10@yahoo.com>
2020-12-09 16:21:23 +0200

    initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,53 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello");
+        System.out.println("1. Hello");
         System.out.println("Mihai");
 
     // Sum of two numbers
         int num1 = 456;
         int num2 = 4356;
         int sum = num1+num2;
-        System.out.println("Suma numerelor 456 si 4356 este " + sum);
+        System.out.println("2. Suma numerelor 456 si 4356 este " + sum);
 
     // Dividing two numbers
+        double num3 = 615;
+        double num4 = 123;
+        System.out.println("3. Rezultatul impartirii este " + num3/num4);
 
+        int num5 = 5;
+        double num6 = 3.12;
+        System.out.println("Rezultatul impartirii este " + num5/num6);
 
+    // The result of following operations
+    //a.
+        int num7 = -5;
+        int num8 = 8;
+        int num9 = 6;
+        int res = num7+num8*num9;
+        System.out.println("4. Rezultatele sunt: ");
+        System.out.println("a." + res);
 
+    //b.
+        int num10 = 55;
+        int num11 = 9;
+        int res1 = (num10 + num11) % num11;
+        System.out.println("b." + res1);
+
+    //c.
+        int num12 = 20;
+        int num13 = -3;
+        int num14 = num5;
+        int res2= num12+num13*num14/num8;
+        System.out.println("c." + res2);
+
+    //d.
+        
 
 
 
